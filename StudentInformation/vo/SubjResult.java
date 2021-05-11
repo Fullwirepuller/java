3055917297d0661f95cd0903eeb8614a26fc0883
Fullwirepuller <73219360+Fullwirepuller@users.noreply.github.com>
2021-05-11 20:14:34 +0800

    Update SubjResult.java

--- StudentInformation/vo/SubjResult.java
+++ StudentInformation/vo/SubjResult.java
@@ -2,15 +2,26 @@ package StudentInformation.vo;
 
 public class SubjResult extends Subject{
 	private float subjAverage;
+	private int count;
 
 	public SubjResult() {
 		super();
 	}
 
-	public SubjResult(float subjAverage) {
+	public SubjResult(String subjName,float subjAverage, int count) {
+		super.setSubjName(subjName);
 		this.subjAverage = subjAverage;
+		this.count = count;
 	}
 	
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
 	public float getSubjAverage() {
 		return subjAverage;
 	}
@@ -21,7 +32,7 @@ public class SubjResult extends Subject{
 
 	@Override
 	public String toString() {
-		return super.toString()+subjAverage;
+		return super.toString()+this.subjAverage;
 	}
 	
 }
