2123559d7df4e9361305fbf5a8059be319b09542
Fullwirepuller <73219360+Fullwirepuller@users.noreply.github.com>
2021-05-11 20:12:36 +0800

    Update StudentDao.java

--- StudentInformation/Dao/StudentDao.java
+++ StudentInformation/Dao/StudentDao.java
@@ -9,128 +9,127 @@ import StudentInformation.vo.StuResult;
 import StudentInformation.vo.SubjResult;
 
 public class StudentDao {
-		public static ArrayList<Student> inputStu(){
-			ArrayList<Student> listStu = new ArrayList<Student>();
-		
-			Scanner scan = new Scanner(System.in);
-			System.out.println("ÇëÊäÈëÑ§Éú»ù±¾ĞÅÏ¢£º");
-			String data="";
-			while(!(data = scan.nextLine()).equals("end")){
-				String array[] = data.split(",|£¬");
-				Student student = new Student(array[0],array[1],array[2]);
-				listStu.add(student);
-			}
-			return listStu;
+	public static ArrayList<Student> inputStu() {
+		ArrayList<Student> listStu = new ArrayList<Student>();
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("è¯·è¾“å…¥å­¦ç”ŸåŸºæœ¬ä¿¡æ¯ï¼š");
+		String data = "";
+		while (!(data = scan.nextLine()).equals("end")) {
+			String array[] = data.split(",|ï¼Œ");
+			Student student = new Student(array[0], array[1], array[2]);
+			listStu.add(student);
 		}
-		
-		public static ArrayList<Subject> inputSubj(){
-			ArrayList<Subject> listSubj = new ArrayList<Subject>();
-			
-			Scanner scan = new Scanner(System.in);
-			System.out.println("ÇëÊäÈëÑ§Éú³É¼¨£º");
-			String data="";
-			while(!(data = scan.nextLine()).equals("end")){
-				String array[] = data.split(",|£¬");
-				float score = Float.parseFloat(array[2]);
-				Subject subject = new Subject(array[0],array[1],score);
-				listSubj.add(subject);
-			}
-			return listSubj;		
+		return listStu;
+	}
+
+	public static ArrayList<Subject> inputSubj() {
+		ArrayList<Subject> listSubj = new ArrayList<Subject>();
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("è¯·è¾“å…¥å­¦ç”Ÿæˆç»©ï¼š");
+		String data = "";
+		while (!(data = scan.nextLine()).equals("end")) {
+			String array[] = data.split(",|ï¼Œ");
+			float score = Float.parseFloat(array[2]);
+			Subject subject = new Subject(array[0], array[1], score);
+			listSubj.add(subject);
 		}
-		
-		public static ArrayList<StuResult> stuAverage(ArrayList<Subject> listSubj){
-			ArrayList<StuResult> stuListResult = new ArrayList<StuResult>();
-			
-			for(int i=0; i<listSubj.size();i++) {
-				Subject subj = listSubj.get(i);
-				String subjId = subj.getStuId();
-				float score = subj.getScore();
-				
-				int index = isId(stuListResult,subjId);
-				int count = 0;
-				if(index!=-1) {
-					count++;
-					StuResult oldResult = stuListResult.get(index);
-					oldResult.setStuAverage((oldResult.getStuAverage()+score)/count);
-					stuListResult.set(index, oldResult);
-				}else {
-					StuResult result = new StuResult(score);
-					stuListResult.add(result);
+		return listSubj;
+	}
+
+	public static ArrayList<StuResult> stuAverage(ArrayList<Student> listStu, ArrayList<Subject> listSubj) {
+		ArrayList<StuResult> stuListResult = new ArrayList<StuResult>();
+
+		for (int i = 0; i < listSubj.size(); i++) {
+			Subject subj = listSubj.get(i);
+			String subjId = subj.getId();
+			float score = subj.getScore();
+
+			String StuId = "";
+			String name = "";
+			String gender = "";
+
+			for (int j = 0; j < listStu.size(); j++) {
+				Student stu = listStu.get(j);
+				StuId = stu.getStuId();
+				if (subjId.equals(StuId)) {
+					name = stu.getName();
+					gender = stu.getGender();
 				}
 			}
-			return stuListResult;
+			int index = isId(stuListResult, subjId);
+			if (index != -1) {
+				StuResult oldResult = stuListResult.get(index);
+				oldResult.setCount((oldResult.getCount())+1);
+				oldResult.setStuAverage(oldResult.getStuAverage() + (score - oldResult.getStuAverage()) / oldResult.getCount());
+				stuListResult.set(index, oldResult);
+			} else {
+				StuResult result = new StuResult(subjId, name, gender, score, 1);
+				stuListResult.add(result);
+			}
 		}
+		return stuListResult;
+	}
+
+	public static ArrayList<SubjResult> subjAverage(ArrayList<Subject> listSubj) {
+		ArrayList<SubjResult> subjListResult = new ArrayList<SubjResult>();
 		
-		public static ArrayList<SubjResult> subjAverage(ArrayList<Subject> listSubj){
-			ArrayList<SubjResult> subjListResult = new ArrayList<SubjResult>();
-			
-			for(int i=0; i<listSubj.size();i++) {
-				Subject subj = listSubj.get(i);
-				String subjName = subj.getSubjName();
-				float score = subj.getScore();
-				
-				int index = isSubjName(subjListResult,subjName);
-				int count = 0;
-				if(index!=-1) {
-					count++;
-					SubjResult oldResult = subjListResult.get(index);
-					oldResult.setScore((oldResult.getScore()+score)/count);
-					subjListResult.set(index, oldResult);
-				}else {
-					SubjResult result = new SubjResult(score);
-					subjListResult.add(result);
-				}
+		for (int i = 0; i < listSubj.size(); i++) {
+			Subject subj = listSubj.get(i);
+			String subjName = subj.getSubjName();
+			float score = subj.getScore();
+
+			int index = isSubjName(subjListResult, subjName);
+			if (index != -1) {
+				SubjResult oldResult = subjListResult.get(index);
+				oldResult.setCount(oldResult.getCount()+1);
+				oldResult.setSubjAverage(oldResult.getSubjAverage() + (score - oldResult.getSubjAverage()) / oldResult.getCount());;
+				subjListResult.set(index, oldResult);
+			} else {
+				SubjResult result = new SubjResult(subjName, score, 1);
+				subjListResult.add(result);
 			}
-			return subjListResult;
 		}
-		
-		public static int isId(ArrayList<StuResult> stuListResult, String id) {
-			int index = -1;
-			for (int i = 0; i < stuListResult.size(); i++ ) {
-				StuResult result = stuListResult.get(i);
-				String Id = result.getStuId();
-				if(Id.equals(id)) {
-					return i;
-				}
+		return subjListResult;
+	}
+
+	public static int isId(ArrayList<StuResult> stuListResult, String id) {
+		int index = -1;
+		for (int i = 0; i < stuListResult.size(); i++) {
+			StuResult result = stuListResult.get(i);
+			String Id = result.getStuId();
+			if (Id.equals(id)) {
+				return i;
 			}
-			return index;
 		}
-		
-		public static int isSubjName(ArrayList<SubjResult> subjListResult, String subjName) {
-			int index = -1;
-			for (int i = 0; i < subjListResult.size(); i++ ) {
-				SubjResult result = subjListResult.get(i);
-				String name = result.getSubjName();
-				if(name.equals(subjName)) {
-					return i;
-				}
+		return index;
+	}
+
+	public static int isSubjName(ArrayList<SubjResult> subjListResult, String subjName) {
+		int index = -1;
+		for (int i = 0; i < subjListResult.size(); i++) {
+			SubjResult result = subjListResult.get(i);
+			String name = result.getSubjName();
+			if (name.equals(subjName)) {
+				return i;
 			}
-			return index;
 		}
-		
-		public static void display(ArrayList<StuResult>stuListResult, ArrayList<SubjResult> subjListResult) {
-			System.out.println("°´Ñ§ÉúÍ³¼Æ£º");
-			System.out.println("Ñ§ºÅ"+"\t"+"ĞÕÃû"+"\t"+"ĞÔ±ğ"+"\t"+"Æ½¾ù·Ö");
-			for (StuResult result : stuListResult) {
-				System.out.println(result.toString());
-			}
-			System.out.println("°´¿Î³ÌÍ³¼Æ£º");
-			System.out.println("¿Î³Ì"+"\t"+"Æ½¾ù·Ö");
-			for (SubjResult result : subjListResult) {
-				System.out.println(result.toString());
-			}
-			
+		return index;
+	}
+
+	public static void display(ArrayList<StuResult> stuListResult, ArrayList<SubjResult> subjListResult) {
+		System.out.println("æŒ‰å­¦ç”Ÿç»Ÿè®¡ï¼š");
+		System.out.println("å­¦å·" + "\t" + "å§“å" + "\t" + "æ€§åˆ«" + "\t" + "å¹³å‡åˆ†");
+		for (StuResult result : stuListResult) {
+			System.out.println(result.toString());
+		}
+		System.out.println("æŒ‰è¯¾ç¨‹ç»Ÿè®¡ï¼š");
+		System.out.println("è¯¾ç¨‹" + "\t" + "å¹³å‡åˆ†");
+		for (SubjResult result : subjListResult) {
+			System.out.println(result.toString());
 		}
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	}
+
 }
